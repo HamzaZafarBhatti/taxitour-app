28cc840fbb49b7a028cd04eb2910c726a14b3671
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2023-06-21 14:52:24 +0500

    Update AssignDriversForRegularRides.php

--- app/Console/Commands/AssignDriversForRegularRides.php
+++ app/Console/Commands/AssignDriversForRegularRides.php
@@ -56,6 +56,7 @@ class AssignDriversForRegularRides extends Command
     public function handle()
     {
         $current_time = Carbon::now()->format('Y-m-d H:i:s');
+        Log::info($uncompleted_requests);
         $sub_5_min = Carbon::now()->subMinutes(20)->format('Y-m-d H:i:s');
         // DB::enableQueryLog();
         $requests = Request::where('is_later', 0)
