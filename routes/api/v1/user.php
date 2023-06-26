b2fdacc9ec57942922b0da37250b22ac779200e5
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2023-06-26 23:49:03 +0500

    Update user.php

--- routes/api/v1/user.php
+++ routes/api/v1/user.php
@@ -16,7 +16,7 @@ use App\Base\Constants\Auth\Role;
  * These routes use the root namespace 'App\Http\Controllers\Api\V1\User'.
  * These routes use the middleware group 'auth'.
  */
-Route::get('/user', 'User/AccountController@me');
+Route::get('/user', 'User\AccountController@me');
 Route::prefix('user')->namespace('User')->middleware('auth')->group(function () {
     // Get the logged in user.
     /**
