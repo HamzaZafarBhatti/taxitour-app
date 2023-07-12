8b507707c4da5e6f97d32eb0643afaf0106290f0
hamzazafarbhatti <hamza0952454@gmail.com>
2023-07-12 13:35:05 +0000

    Adminbankaccount module

---
+++ app/Http/Controllers/AdminBankAccountsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AdminBankAccounts;
+use Illuminate\Http\Request;
+
+class AdminBankAccountsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AdminBankAccounts  $adminBankAccounts
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AdminBankAccounts $adminBankAccounts)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AdminBankAccounts  $adminBankAccounts
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AdminBankAccounts $adminBankAccounts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AdminBankAccounts  $adminBankAccounts
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AdminBankAccounts $adminBankAccounts)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AdminBankAccounts  $adminBankAccounts
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AdminBankAccounts $adminBankAccounts)
+    {
+        //
+    }
+}
